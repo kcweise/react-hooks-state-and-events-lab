cdfd5fc7e8f3fcd280b1dcbdb84c0c21256e3fa1
Keenan Weise <kcweise@gmail.com>
2024-03-16 11:35:35 -0500

    Lab completion

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  
+  const [inCart, setInCart] = useState(false)
+  const liClass = inCart ? "in-cart": "";
+  const buttonText = inCart ? "Remove From Cart":"Add to Cart";
+
+  function clickHandler() {
+    setInCart((inCart)=>!inCart)
+
+  }
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick = {clickHandler} className="add">{buttonText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory]= useState("All")
+
+
+  function handleClick(e){
+    setSelectedCategory((selectedCategory)=>selectedCategory = e.target.value)
+  
+  }
+  {/*This worked replacing items.map in the ul, but didn't pass test
+  const filteredItems = items.filter((item)=>{
+    return selectedCategory === "All" || item.category === selectedCategory;
+  })
+*/}
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onClick = {handleClick}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,8 +26,8 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
+        {items.filter((item) => selectedCategory === "All" || item.category === selectedCategory)
+          .map((item)=> (<Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
